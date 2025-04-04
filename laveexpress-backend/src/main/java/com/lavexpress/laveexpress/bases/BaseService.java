a115edf9b90467538257d568f4bb8195698ff957
andre.guil <andre.guil@placeti.com.br>
2025-04-04 15:30:48 -0300

    feat (backend) adicionar base service, dev profile e SecurityConfig

--- laveexpress-backend/src/main/java/com/lavexpress/laveexpress/bases/BaseService.java
+++ laveexpress-backend/src/main/java/com/lavexpress/laveexpress/bases/BaseService.java
@@ -1,7 +1,40 @@
 package com.lavexpress.laveexpress.bases;
 
-import jakarta.transaction.Transactional;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional
-public class BaseService {
+public abstract class BaseService<Entity> {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    public abstract JpaRepository<Entity, Long> getRepository();
+
+    public void save(Entity entity) {
+        getRepository().save(entity);
+    }
+
+    public Entity saveWithReturn(Entity entity) {
+        return getRepository().save(entity);
+    }
+
+    public void delete(Entity entity) {
+        getRepository().delete(entity);
+    }
+
+//    public abstract Page<Entity> findAll(int page, int size, EntityFilter filter);
+
+    public Optional<Entity> findById(Long id) {
+        return getRepository().findById(id);
+    }
+
+
+
 }
--- laveexpress-backend/src/main/java/com/lavexpress/laveexpress/services/UsuarioService.java
+++ laveexpress-backend/src/main/java/com/lavexpress/laveexpress/services/UsuarioService.java
@@ -1,4 +1,33 @@
 package com.lavexpress.laveexpress.services;
 
-public class UsuarioService {
+import com.lavexpress.laveexpress.bases.BaseService;
+import com.lavexpress.laveexpress.dtos.UsuarioDto;
+import com.lavexpress.laveexpress.entities.Usuario;
+import com.lavexpress.laveexpress.mappers.UsuarioMapper;
+import com.lavexpress.laveexpress.repositories.UsuarioRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UsuarioService extends BaseService<Usuario> {
+
+    private final UsuarioRepository usuarioRepository;
+
+    private final UsuarioMapper mapper;
+
+    public UsuarioService(UsuarioRepository usuarioRepository, UsuarioMapper mapper) {
+        this.usuarioRepository = usuarioRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public JpaRepository<Usuario, Long> getRepository() {
+        return usuarioRepository;
+    }
+
+    public Usuario create(UsuarioDto usuarioDto) {
+        var usuario = mapper.dtoToEntity(usuarioDto);
+        return usuarioRepository.save(usuario);
+    }
+
+
+
 }
